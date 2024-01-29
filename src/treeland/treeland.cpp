8170f10a7aba513e4a7b027e67b8a69e843d8260
ComixHe <heyuming@deepin.org>
2024-01-29 15:26:57 +0800

    feat: support run a process after treeland started
    Signed-off-by: ComixHe <heyuming@deepin.org>

--- src/treeland/treeland.cpp
+++ src/treeland/treeland.cpp
@@ -34,6 +34,7 @@
 #include <QQuickStyle>
 
 #include <pwd.h>
+#include <wordexp.h>
 
 Q_IMPORT_PLUGIN(TreeLand_ProtocolsPlugin)
 Q_IMPORT_PLUGIN(TreeLand_UtilsPlugin)
@@ -41,6 +42,99 @@ Q_IMPORT_PLUGIN(TreeLand_UtilsPlugin)
 Q_LOGGING_CATEGORY(debug, "treeland.kernel.debug", QtDebugMsg);
 
 using namespace SDDM;
+
+QString unescape(const QString &str) noexcept
+{
+    QString unescapedStr;
+    for (qsizetype i = 0; i < str.size(); ++i) {
+        auto c = str.at(i);
+        if (c != '\\') {
+            unescapedStr.append(c);
+            continue;
+        }
+
+        switch (str.at(i + 1).toLatin1()) {
+        default:
+            unescapedStr.append(c);
+            break;
+        case 'n':
+            unescapedStr.append('\n');
+            ++i;
+            break;
+        case 't':
+            unescapedStr.append('\t');
+            ++i;
+            break;
+        case 'r':
+            unescapedStr.append('\r');
+            ++i;
+            break;
+        case '\\':
+            unescapedStr.append('\\');
+            ++i;
+            break;
+        case ';':
+            unescapedStr.append(';');
+            ++i;
+            break;
+        case 's': {
+            unescapedStr.append(R"(\ )");
+            ++i;
+        } break;
+        }
+    }
+
+    return unescapedStr;
+}
+
+std::optional<QStringList> unescapeExecArgs(const QString &str) noexcept
+{
+    auto unescapedStr = unescape(str);
+    if (unescapedStr.isEmpty()) {
+        qWarning() << "unescape Exec failed.";
+        return std::nullopt;
+    }
+
+    auto deleter = [](wordexp_t *word) {
+        wordfree(word);
+        delete word;
+    };
+    std::unique_ptr<wordexp_t, decltype(deleter)> words{new (std::nothrow) wordexp_t{0, nullptr, 0}, deleter};
+
+    if (auto ret = wordexp(unescapedStr.toLocal8Bit(), words.get(), WRDE_SHOWERR); ret != 0) {
+        if (ret != 0) {
+            QString errMessage;
+            switch (ret) {
+            case WRDE_BADCHAR:
+                errMessage = "BADCHAR";
+                break;
+            case WRDE_BADVAL:
+                errMessage = "BADVAL";
+                break;
+            case WRDE_CMDSUB:
+                errMessage = "CMDSUB";
+                break;
+            case WRDE_NOSPACE:
+                errMessage = "NOSPACE";
+                break;
+            case WRDE_SYNTAX:
+                errMessage = "SYNTAX";
+                break;
+            default:
+                errMessage = "unknown";
+            }
+            qWarning() << "wordexp error: " << errMessage;
+            return std::nullopt;
+        }
+    }
+
+    QStringList execList;
+    for (std::size_t i = 0; i < words->we_wordc; ++i) {
+      execList.emplace_back(words->we_wordv[i]);
+    }
+
+    return execList;
+}
 
 namespace TreeLand {
 TreeLand::TreeLand(TreeLandAppContext context)
@@ -62,23 +156,47 @@ TreeLand::TreeLand(TreeLandAppContext context)
 
     setup();
 
-    if (!context.socket.isEmpty()) {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
-        Helper *helper = m_engine->singletonInstance<Helper*>("TreeLand.Utils", "Helper");
+    Helper *helper = m_engine->singletonInstance<Helper *>("TreeLand.Utils", "Helper");
 #else
-        auto helperTypeId = qmlTypeId("TreeLand.Utils", 1, 0, "Helper");
-        Helper *helper = m_engine->singletonInstance<Helper*>(helperTypeId);
+    auto helperTypeId = qmlTypeId("TreeLand.Utils", 1, 0, "Helper");
+    Helper *helper = m_engine->singletonInstance<Helper *>(helperTypeId);
 #endif
+
+    if (!context.socket.isEmpty()) {
         Q_ASSERT(helper);
-        auto connectToServer = [this, context] {
-            m_socket->connectToServer(context.socket);
-        };
+        auto connectToServer = [this, context] { m_socket->connectToServer(context.socket); };
 
         connect(helper, &Helper::socketFileChanged, this, connectToServer);
 
         if (!helper->socketFile().isEmpty()) {
             connectToServer();
         }
+    }
+
+    if (!context.run.isEmpty()) {
+      qInfo() << "run cmd:" << context.run;
+      auto exec = [runCmd = context.run, helper] {
+        if (auto cmdline = unescapeExecArgs(runCmd); cmdline) {
+          auto cmdArgs = cmdline.value();
+
+          auto envs = QProcessEnvironment::systemEnvironment();
+          envs.insert("WAYLAND_DISPLAY", helper->socketFile());
+
+          QProcess process;
+          process.setProgram(cmdArgs.constFirst());
+          process.setArguments(cmdArgs.mid(1));
+          process.setProcessEnvironment(envs);
+          process.startDetached();
+        }
+      };
+      auto con = connect(helper, &Helper::socketFileChanged, this, exec,
+                         Qt::SingleShotConnection);
+
+      if (!helper->socketFile().isEmpty()) {
+        QObject::disconnect(con);
+        exec();
+      }
     }
 }
 
@@ -236,16 +354,15 @@ int main (int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QCommandLineOption socket({"s", "socket"}, "set ddm socket", "socket");
+    QCommandLineOption run({"r", "run"}, "run a process", "run");
 
     QCommandLineParser parser;
     parser.addHelpOption();
-    parser.addOption(socket);
+    parser.addOptions({socket, run});
 
     parser.process(app);
 
-    TreeLand::TreeLand treeland({
-        parser.value(socket),
-    });
+    TreeLand::TreeLand treeland({parser.value(socket), parser.value(run)});
 
     return app.exec();
 }
