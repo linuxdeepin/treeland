2df7a798d69f4a3441c4bc9f089625b53ab4bdd9
rubbishzhyic <zhongyicheng@uniontech.com>
2024-04-01 09:57:51 +0800

    feat: dock adapt surfaces model
    refactor using a generic filterModel;
    fix crash when previewing on surface exit;
    fix type errors about `surface`or `surfaceItem`

---
+++ src/treeland/quick/utils/filterproxymodel.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2024 Yicheng Zhong <zhongyicheng@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "filterproxymodel.h"
+#include <QQmlEngine>
+
+FilterProxyModel::FilterProxyModel(QObject *parent)
+    : QSortFilterProxyModel{parent}
+{
+    connect(this, &QSortFilterProxyModel::layoutChanged, this, &FilterProxyModel::countChanged);
+}
+
+bool FilterProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
+{
+    Q_UNUSED(source_parent);
+    auto rowData = QVariantMap();
+    for (auto role: roleNames().asKeyValueRange())
+        rowData.insert(role.second, sourceModel()->data(sourceModel()->index(source_row, 0), role.first));
+    auto jsData = qjsEngine(this)->toScriptValue(rowData);
+    return m_filterAcceptsRow.call(QJSValueList({jsData})).toBool();
+}
+void FilterProxyModel::setFilterAcceptsRow(const QJSValue &val)
+{
+    if (val.equals(m_filterAcceptsRow))
+        return;
+    m_filterAcceptsRow = val;
+    emit filterAcceptsRowChanged();
+}
+
+QVariantMap FilterProxyModel::get(const int index) const
+{
+    auto rowData = QVariantMap();
+    for (auto role: roleNames().asKeyValueRange())
+        rowData.insert(role.second, data(this->index(index, 0), role.first));
+    return rowData;
+}
+
+void FilterProxyModel::invalidate()
+{
+    invalidateFilter();
+}
+int FilterProxyModel::count() const
+{
+    return rowCount();
+}
