3f6c3cfcfe0d45325b27e60672f678a37e853b9f
rewine <luhongxu@deepin.org>
2024-06-21 13:34:47 +0800

    chore: use BUILD_TEST_CLIENTS to build tests demo
    log:

--- tests/test_wallpaper_color/main.cpp
+++ tests/test_wallpaper_color/main.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2024 rewine <luhongxu@deepin.com>.
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include "qwayland-treeland-wallpaper-color.h"
+#include "qwayland-treeland-wallpaper-color-v1.h"
 
 #include <private/qwaylandwindow_p.h>
 
