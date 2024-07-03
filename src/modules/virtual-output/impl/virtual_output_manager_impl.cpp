70e7102ab4d700b5f85dc56ea6fe8925b276752b
Lu YaNing <luyaning@uniontech.com>
2024-07-03 15:19:09 +0800

    feat: Implement treeland_virtual_manager_v1 function
    Implementing Virtual Output

---
+++ src/modules/virtual-output/impl/virtual_output_manager_impl.cpp
@@ -0,0 +1,241 @@
+// Copyright (C) 2024 Lu YaNing <luyaning@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "virtual_output_manager_impl.h"
+
+#include <wayland-server-core.h>
+#include <cassert>
+
+#include <QDebug>
+
+#define TREELAND_VIRTUAL_OUTPUT_MANAGER_V1_VERSION 1
+
+using QW_NAMESPACE::QWDisplay;
+
+static treeland_virtual_output_manager_v1 *virtual_output_manager_from_resource(wl_resource *resource);
+static void virtual_output_manager_bind(wl_client *client, void *data, uint32_t version, uint32_t id);
+
+static void virtual_output_manager_handle_create_virtual_output([[maybe_unused]]struct wl_client *client,
+                                                                         struct wl_resource *manager_resource,
+                                                                         uint32_t id,
+                                                                         const char *name,
+                                                                         struct wl_array *outputs);
+
+static void virtual_output_manager_handle_get_virtual_output_list([[maybe_unused]]struct wl_client *client, struct wl_resource *resource);
+
+static void virtual_output_manager_handle_get_virtual_output([[maybe_unused]]struct wl_client *client,
+                                                                      struct wl_resource *resource,
+                                                                      const char *name,
+                                                                      uint32_t id);
+
+static void virtual_output_handle_destroy([[maybe_unused]]struct wl_client *client,
+                                                   struct wl_resource *resource);
+static const struct virtual_output_v1_interface virtual_output_impl {
+    .destroy = virtual_output_handle_destroy,
+};
+
+
+static const struct virtual_output_manager_v1_interface virtual_output_manager_impl {
+    .create_virtual_output = virtual_output_manager_handle_create_virtual_output,
+    .get_virtual_output_list = virtual_output_manager_handle_get_virtual_output_list,
+    .get_virtual_output = virtual_output_manager_handle_get_virtual_output,
+};
+
+static struct treeland_virtual_output_v1 *virtual_output_from_resource(wl_resource *resource)
+{
+    assert(wl_resource_instance_of(resource,
+                                   &virtual_output_v1_interface,
+                                   &virtual_output_impl));
+    auto *manager = static_cast<treeland_virtual_output_v1 *>(wl_resource_get_user_data(resource));
+    assert(manager != nullptr);
+    return manager;
+}
+
+static treeland_virtual_output_manager_v1 *virtual_output_manager_from_resource(wl_resource *resource)
+{
+    assert(wl_resource_instance_of(resource,
+                                   &virtual_output_manager_v1_interface,
+                                   &virtual_output_manager_impl));
+    auto *manager = static_cast<treeland_virtual_output_manager_v1 *>(wl_resource_get_user_data(resource));
+    assert(manager != nullptr);
+    return manager;
+}
+
+static void virtual_output_handle_destroy([[maybe_unused]]struct wl_client *client,
+                                                   struct wl_resource *resource)
+{
+
+    auto *context = virtual_output_from_resource(resource);
+    for (auto *virtual_output : context->manager->virtual_output) {
+        if (resource == virtual_output->resource)
+            Q_EMIT context->manager->virtualOutputDestroy(virtual_output);
+    }
+    wl_resource_destroy(resource);
+}
+
+static void virtual_output_resource_destroy(struct wl_resource *resource)
+{
+    auto *manager = virtual_output_from_resource(resource);
+    if (!manager) {
+        return;
+    }
+
+    delete manager;
+}
+
+static void virtual_output_manager_resource_destroy(struct wl_resource *resource)
+{
+    wl_list_remove(wl_resource_get_link(resource));
+}
+
+void wlarrayToStringList(const wl_array* wl_array, QStringList& stringList)
+{
+    char* dataStart = static_cast<char*>(wl_array->data);
+    char* currentPos = dataStart;
+
+    while (*currentPos != '\0') {
+        QString str = QString::fromUtf8(currentPos);
+        stringList << str;
+        currentPos += str.toLocal8Bit().length() + 1;
+    }
+}
+
+static void virtual_output_manager_handle_create_virtual_output([[maybe_unused]]struct wl_client *client,
+                                                                         struct wl_resource *manager_resource,
+                                                                         uint32_t id,
+                                                                         const char *name,
+                                                                         struct wl_array *outputs)
+{
+    auto *manager = virtual_output_manager_from_resource(manager_resource);
+
+    auto *virtual_output = new treeland_virtual_output_v1;
+    if (virtual_output == nullptr) {
+        wl_resource_post_no_memory(manager_resource);
+        return;
+    }
+
+    uint32_t version = wl_resource_get_version(manager_resource);
+    struct wl_resource *resource =
+        wl_resource_create(client, &virtual_output_v1_interface, version, id);
+    if (resource == nullptr) {
+        delete virtual_output;
+        wl_resource_post_no_memory(manager_resource);
+        return;
+    }
+
+    wl_resource_set_implementation(resource,
+                                   &virtual_output_impl,
+                                   virtual_output,
+                                   virtual_output_resource_destroy);
+    wl_resource_set_user_data(resource, virtual_output);
+
+    virtual_output->manager = manager;
+    virtual_output->resource = resource;
+    virtual_output->name = name;
+    virtual_output->screen_outputs = outputs;
+
+    wlarrayToStringList(outputs,virtual_output->outputList);
+
+    manager->virtual_output.append(virtual_output);
+    QObject::connect(virtual_output, &treeland_virtual_output_v1::beforeDestroy, manager, [manager, virtual_output]() {
+        manager->virtual_output.removeOne(virtual_output);
+    });
+
+    virtual_output->send_outputs(virtual_output->name, outputs);
+    Q_EMIT virtual_output->manager->virtualOutputCreated(virtual_output);
+}
+
+static void virtual_output_manager_handle_get_virtual_output_list([[maybe_unused]]struct wl_client *client, struct wl_resource *resource)
+{
+    auto *manager = virtual_output_manager_from_resource(resource);
+
+    wl_array arr;
+    wl_array_init(&arr);
+    for(int i = 0; i < manager->virtual_output.size(); ++i) {
+        treeland_virtual_output_v1 *virtual_output = manager->virtual_output.at(i);
+        char *dest = static_cast<char *>(wl_array_add(&arr, virtual_output->name.length() + 1));
+        strncpy(dest, virtual_output->name.toLatin1().data(), static_cast<uint>(virtual_output->name.length()));
+    }
+
+    virtual_output_manager_v1_send_virtual_output_list(resource,&arr);
+}
+
+static void virtual_output_manager_handle_get_virtual_output([[maybe_unused]]struct wl_client *client,
+                                                                      struct wl_resource *resource,
+                                                                      const char *name,
+                                                                      uint32_t id)
+{
+    auto *manager = virtual_output_manager_from_resource(resource);
+    for(int i = 0; i < manager->virtual_output.size(); ++i) {
+        treeland_virtual_output_v1 *virtual_output = manager->virtual_output.at(i);
+        if (virtual_output->name == name) {
+            virtual_output->send_outputs(name, virtual_output->screen_outputs);
+        }
+    }
+}
+
+void treeland_virtual_output_v1::send_outputs(QString name, struct wl_array *outputs)
+{
+    virtual_output_v1_send_outputs(resource, name.toLocal8Bit().data(), outputs);
+}
+
+void treeland_virtual_output_v1::send_error(uint32_t code, const char *message)
+{
+    virtual_output_v1_send_error(resource, code, message);
+}
+
+treeland_virtual_output_v1::~treeland_virtual_output_v1()
+{
+    Q_EMIT beforeDestroy();
+    if (resource)
+        wl_resource_set_user_data(resource,nullptr);
+}
+
+treeland_virtual_output_manager_v1::~treeland_virtual_output_manager_v1()
+{
+    Q_EMIT beforeDestroy();
+    if (global)
+        wl_global_destroy(global);
+}
+
+static void virtual_output_manager_bind(wl_client *client, void *data, uint32_t version, uint32_t id)
+{
+    auto *manager = static_cast<treeland_virtual_output_manager_v1 *>(data);
+
+    struct wl_resource *resource =
+        wl_resource_create(client, &virtual_output_manager_v1_interface, version, id);
+    if (!resource) {
+        wl_client_post_no_memory(client);
+        return;
+    }
+
+    wl_resource_set_implementation(resource,
+                                   &virtual_output_manager_impl,
+                                   manager,
+                                   virtual_output_manager_resource_destroy);
+    wl_list_insert(&manager->resources, wl_resource_get_link(resource));
+}
+
+treeland_virtual_output_manager_v1 *treeland_virtual_output_manager_v1::create(QWDisplay *display)
+{
+    auto *manager = new treeland_virtual_output_manager_v1;
+    if (!manager)
+        return nullptr;
+
+    manager->event_loop = wl_display_get_event_loop(display->handle());
+    manager->global = wl_global_create(display->handle(),
+                                       &virtual_output_manager_v1_interface,
+                                       TREELAND_VIRTUAL_OUTPUT_MANAGER_V1_VERSION,
+                                       manager,
+                                       virtual_output_manager_bind);
+    if (!manager->global) {
+        delete manager;
+        return nullptr;
+    }
+
+    wl_list_init(&manager->resources);
+
+    connect(display, &QWDisplay::beforeDestroy, manager, [manager] { delete manager; });
+
+    return manager;
+}
---
+++ src/modules/virtual-output/virtualoutputmanager.cpp
@@ -0,0 +1,226 @@
+// Copyright (C) 2024 Lu YaNing <luyaning@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+#include "impl/virtual_output_manager_impl.h"
+
+#include "virtualoutputmanager.h"
+
+#include <wserver.h>
+
+#include <qwdisplay.h>
+#include <qwsignalconnector.h>
+#include <woutput.h>
+#include <woutputitem.h>
+#include <wquickoutputlayout.h>
+#include <QQmlInfo>
+
+extern "C" {
+#include <wayland-server-core.h>
+}
+
+static VirtualOutputV1 *VIRTUAL_OUTPUT = nullptr;
+
+VirtualOutputManagerAttached::VirtualOutputManagerAttached(
+    WOutputViewport *outputviewport, VirtualOutputV1 *virtualOutput)
+    : QObject(outputviewport)
+    , m_viewport(outputviewport)
+    , m_backviewport(outputviewport)
+    , m_virtualoutput(virtualOutput)
+{
+    m_virtualoutput->m_viewports_list.append(outputviewport);
+    connect(m_virtualoutput,
+            &VirtualOutputV1::requestCreateVirtualOutput,
+            this,
+            [this](QString name, QStringList outputList) {
+                if (outputList.contains(m_viewport->output()->name()) && m_viewport->output()->name() != outputList.at(0)) {
+                    // todo: Replication mode Enable the soft cursor and wait for the hard cursor to be reconstructed
+                    m_virtualoutput->m_viewports_list.at(0)->output()->setForceSoftwareCursor(true);
+
+                    WQuickOutputLayout *outputlayout = static_cast<WQuickOutputLayout*>(m_viewport->output()->layout());
+                    WOutputItem *item = WOutputItem::getOutputItem(m_viewport->output());
+                    outputlayout->remove(item);
+
+                    m_viewport = m_virtualoutput->m_viewports_list.at(0);
+
+                    Q_EMIT outputViewportChanged();
+                }
+
+            });
+
+    connect(m_virtualoutput,
+            &VirtualOutputV1::destroyVirtualOutput,
+            this,
+            [this](QString name, QStringList outputList) {
+                if (outputList.contains(m_backviewport->output()->name()) && m_backviewport->output()->name() != outputList.at(0)) {
+                    m_viewport->output()->setForceSoftwareCursor(false);
+
+                    WQuickOutputLayout *outputlayout = static_cast<WQuickOutputLayout*>(m_viewport->output()->layout());
+                    WOutputItem *item = WOutputItem::getOutputItem(m_backviewport->output());
+                    outputlayout->add(item);
+                    m_viewport = m_backviewport;
+
+                    Q_EMIT outputViewportChanged();
+                }
+
+            });
+
+    connect(m_virtualoutput,
+            &VirtualOutputV1::removeVirtualOutput,
+            this,
+            [this](QStringList outputList, WOutput *output) {
+                if (output->name() != m_backviewport->output()->name() && m_viewport == m_backviewport) {
+                    if (m_viewport == m_backviewport)
+                        return;
+
+                    m_viewport = m_backviewport;
+                    m_viewport->setInput(nullptr);
+                    Q_EMIT outputViewportChanged();
+                }
+            });
+    connect(m_virtualoutput,
+            &VirtualOutputV1::newVirtualOutput,
+            this,
+            [this](QStringList outputList, WOutput *output) {
+                if (m_viewport->output() == output) {
+                    if (m_viewport == m_virtualoutput->m_viewports_list.at(0))
+                        return;
+                    m_virtualoutput->m_viewports_list.at(0)->output()->setForceSoftwareCursor(true);
+
+                    WQuickOutputLayout *outputlayout = static_cast<WQuickOutputLayout*>(m_viewport->output()->layout());
+                    WOutputItem *item = WOutputItem::getOutputItem(m_viewport->output());
+                    outputlayout->remove(item);
+
+                    m_viewport = m_virtualoutput->m_viewports_list.at(0);
+                    Q_EMIT outputViewportChanged();
+                }
+            });
+}
+
+VirtualOutputV1::VirtualOutputV1(QObject *parent)
+{
+    if (VIRTUAL_OUTPUT) {
+        qFatal("There are multiple instances of VirtualOutputV1");
+    }
+
+    VIRTUAL_OUTPUT = this;
+}
+
+void VirtualOutputV1::onVirtualOutputCreated(treeland_virtual_output_v1 *virtual_output)
+{
+    m_virtualOutputv1.append(virtual_output);
+    connect(virtual_output, &treeland_virtual_output_v1::beforeDestroy, this, [this, virtual_output] {
+        m_virtualOutputv1.removeOne(virtual_output);
+    });
+
+    if (virtual_output->name.isEmpty()) {
+        virtual_output->send_error(VIRTUAL_OUTPUT_V1_ERROR_INVALID_GROUP_NAME,"Group name is empty!");
+        return;
+    }
+
+    if (virtual_output->outputList.count() < 2) {
+        virtual_output->send_error(VIRTUAL_OUTPUT_V1_ERROR_INVALID_SCREEN_NUMBER,"The number of screens applying for copy mode is less than 2!");
+        return;
+    } else {
+        QStringList outputList;
+        for (auto *outputviewport : m_viewports_list) {
+            outputList.append(outputviewport->output()->name());
+        }
+
+        for (const QString& output : virtual_output->outputList) {
+            if (!outputList.contains(output)) {
+                QString screen = output + " does not exist!";
+                virtual_output->send_error(VIRTUAL_OUTPUT_V1_ERROR_INVALID_OUTPUT,screen.toLocal8Bit().data());
+                return;
+            }
+        }
+    }
+
+    Q_EMIT requestCreateVirtualOutput(virtual_output->name, virtual_output->outputList);
+}
+
+void VirtualOutputV1::onVirtualOutputDestroy(treeland_virtual_output_v1 *virtual_output)
+{
+    QList<treeland_virtual_output_v1 *>::iterator it = m_virtualOutputv1.begin();
+    while (it != m_virtualOutputv1.end()) {
+        if (*it == virtual_output) {
+            it = m_virtualOutputv1.erase(it);
+            Q_EMIT destroyVirtualOutput(virtual_output->name, virtual_output->outputList);
+        } else {
+            ++it;
+        }
+    }
+}
+
+VirtualOutputManagerAttached *VirtualOutputV1::Attach(WOutputViewport *outputviewport)
+{
+    if (outputviewport) {
+        return new VirtualOutputManagerAttached(outputviewport, VIRTUAL_OUTPUT);
+    }
+
+    return nullptr;
+}
+
+void VirtualOutputV1::setVirtualOutput(QString name, QStringList outputList)
+{
+    if (name.isEmpty()) {
+        return;
+    }
+
+    Q_EMIT requestCreateVirtualOutput(name, outputList);
+}
+
+void VirtualOutputV1::newOutput(WOutput *output)
+{
+    if (!m_manager) {
+        return;
+    }
+
+    for (auto *virtualOutput : m_virtualOutputv1) {
+        if (virtualOutput->outputList.contains(output->name())){
+            Q_EMIT newVirtualOutput(virtualOutput->outputList, output);
+        }
+    }
+}
+
+void VirtualOutputV1::removeOutput(WOutput *output)
+{
+    if (!m_manager) {
+        return;
+    }
+
+    for (auto *virtualOutput : m_virtualOutputv1) {
+        if (virtualOutput->outputList.contains(output->name())){
+            Q_EMIT removeVirtualOutput(virtualOutput->outputList, output);
+        }
+    }
+
+    for (auto *outputviewport : m_viewports_list) {
+        if (outputviewport->output() == output){
+            m_viewports_list.removeOne(outputviewport);
+        }
+    }
+}
+
+QByteArrayView VirtualOutputV1::interfaceName() const
+{
+    return virtual_output_manager_v1_interface.name;
+}
+
+void VirtualOutputV1::create(WServer *server)
+{
+    m_manager = treeland_virtual_output_manager_v1::create(server->handle());
+    connect(m_manager,
+            &treeland_virtual_output_manager_v1::virtualOutputCreated,
+            this,
+            &VirtualOutputV1::onVirtualOutputCreated);
+    connect(m_manager,
+            &treeland_virtual_output_manager_v1::virtualOutputDestroy,
+            this,
+            &VirtualOutputV1::onVirtualOutputDestroy);
+}
+
+void VirtualOutputV1::destroy(WServer *server) { }
+
+wl_global *VirtualOutputV1::global() const
+{
+    return m_manager->global;
+}
