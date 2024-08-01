77a694926a8e9bea894348b8ebcbe63995432bf2
pengwenhao <pengwenhao@uniontech.com>
2024-08-01 14:32:21 +0800

    feat: fast togglable gesture state
    Signed-off-by: pengwenhao <pengwenhao@uniontech.com>

---
+++ src/utils/togglablegesture.h
@@ -0,0 +1,66 @@
+// Copyright (C) 2024 WenHao Peng <pengwenhao@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <QObject>
+#include "gestures.h"
+
+class TogglableGesture : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool inProgress READ inProgress WRITE setInProgress NOTIFY inProgressChanged FINAL)
+    Q_PROPERTY(qreal partialGestureFactor READ partialGestureFactor WRITE setPartialGestureFactor NOTIFY partialGestureFactorChanged FINAL)
+public:
+    enum Status {
+        Inactive,
+        Activating,
+        Deactivating,
+        Active,
+        Stopped
+    };
+    Q_ENUM(Status);
+
+    TogglableGesture(QObject *parent = nullptr);
+    ~TogglableGesture();
+
+    bool inProgress() const;
+    void setInProgress(bool gesture);
+
+    qreal partialGestureFactor() const { return m_partialGestureFactor; }
+    void setPartialGestureFactor(qreal factor);
+
+    void activate();
+    void deactivate();
+    void toggle();
+    void stop();
+    void setStatus(Status status);
+    Status status() const { return m_status; }
+
+    void addTouchpadSwipeGesture(SwipeGesture::Direction direction, uint fingerCount);
+
+Q_SIGNALS:
+    void inProgressChanged();
+    void partialGestureFactorChanged(qreal factor);
+    void activated();
+    void deactivated();
+    void statusChanged(Status status);
+
+protected:
+    std::function<void(qreal progress)> progressCallback();
+    std::function<void(qreal progress)> regressCallback();
+    void setProgress(qreal progress);
+    void setRegress(qreal regress);
+
+    std::function<void()> activeTriggeredCallback();
+    std::function<void()> deactivateTriggeredCallback();
+    void activeTriggered();
+    void deactivateTriggered();
+
+private:
+    Status m_status = Status::Inactive;
+    bool m_inProgress = false;
+    qreal m_partialGestureFactor;
+};
+
+Q_DECLARE_METATYPE(TogglableGesture*)
