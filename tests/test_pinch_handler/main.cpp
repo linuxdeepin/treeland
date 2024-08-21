4adc3f373bfc82a477d8fe12f8ae13efa2434742
groveer <guoyao@uniontech.com>
2024-08-21 19:04:29 +0800

    feat: add pinch handler test
    Log: 用于测试二指手势

---
+++ tests/test_pinch_handler/main.cpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_QPA_PLATFORM", "wayland");
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
+    const QUrl url(u"qrc:/qt/qml/pinchhandler/Main.qml"_qs);
+#else
+    const QUrl url(u"qrc:/pinchhandler/Main.qml"_qs);
+#endif
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() {
+            QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
