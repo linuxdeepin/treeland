795d46cfa0af6735813a4f65d7cd1cf5a0f3d994
Yixue Wang <wangyixue@deepin.org>
2024-09-18 11:22:29 +0800

    chore: upgrade licenses
    Upgrade reuse license method, add some missing copyright and licenses.

--- tests/test_data_control_manager/main.cpp
+++ tests/test_data_control_manager/main.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
 #include "clipboard.h"
 
 #include <QGuiApplication>
