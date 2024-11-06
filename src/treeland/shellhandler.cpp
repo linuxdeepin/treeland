11cf97740b20e9907d84201ce7a285627207e6a9
Yixue Wang <wangyixue@deepin.org>
2024-11-06 17:08:36 +0800

    fix: license and header protection

--- src/treeland/shellhandler.cpp
+++ src/treeland/shellhandler.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2024 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
 #include "shellhandler.h"
 
 #include "ddeshellmanagerv1.h"
