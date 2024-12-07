1861662084a1513ec52312cb05bc282feca7c46b
pengwenhao <pengwenhao@uniontech.com>
2024-12-07 14:38:59 +0800

    feat: sort the task switcher according to active order
    Signed-off-by: pengwenhao <pengwenhao@uniontech.com>

--- src/core/surfacefilterproxymodel.cpp
+++ src/core/surfacefilterproxymodel.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "surfacefilterproxymodel.h"
-
+#include "workspacemodel.h"
 #include "surfacewrapper.h"
 
 SurfaceFilterProxyModel::SurfaceFilterProxyModel(QObject *parent)
@@ -46,4 +46,24 @@ bool SurfaceFilterProxyModel::filterAcceptsRow(int source_row,
     }
 
     return false;
+}
+
+bool SurfaceFilterProxyModel::lessThan(const QModelIndex &source_left,
+                                       const QModelIndex &source_right) const
+{
+    WorkspaceModel *model = dynamic_cast<WorkspaceModel *>(sourceModel());
+    SurfaceWrapper *left_surface = sourceModel()->data(source_left).value<SurfaceWrapper *>();
+    SurfaceWrapper *right_surface = sourceModel()->data(source_right).value<SurfaceWrapper *>();
+
+    if (model && left_surface && right_surface) {
+        int left_index = model->findActivedSurfaceHistoryIndex(left_surface);
+        int right_index = model->findActivedSurfaceHistoryIndex(right_surface);
+
+        if (left_index > right_index)
+            return true;
+        else
+            return false;
+    }
+
+    return QSortFilterProxyModel::lessThan(source_left, source_right);
 }
