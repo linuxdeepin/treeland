a823619a1ed007423da72311aac4c1854e0e2dde
JiDe Zhang <zhangjide@deepin.org>
2025-02-11 17:19:36 +0800

    fix: can't start on VirtualBox
    This is a workaround.
    On the VirtualBox no 3D accelerate, eglInitialize will crash.
    
    Depends https://github.com/vioken/waylib/pull/615

--- src/utils/cmdline.h
+++ src/utils/cmdline.h
@@ -4,13 +4,13 @@
 #include <DSingleton>
 
 #include <QObject>
+#include <QCommandLineOption>
 
 #include <memory>
 #include <optional>
 
 class QCoreApplication;
 class QCommandLineParser;
-class QCommandLineOption;
 
 DCORE_USE_NAMESPACE
 
@@ -25,6 +25,7 @@ public:
     std::optional<QString> run() const;
     bool useLockScreen() const;
     std::optional<QStringList> unescapeExecArgs(const QString &str) noexcept;
+    bool tryExec() const;
 
 private:
     CmdLine();
@@ -35,4 +36,5 @@ private:
     std::unique_ptr<QCommandLineParser> m_parser;
     std::unique_ptr<QCommandLineOption> m_run;
     std::unique_ptr<QCommandLineOption> m_lockScreen;
+    QCommandLineOption m_tryExec;
 };
