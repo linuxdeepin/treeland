1570936d55e9b1db478407c229b79898ac5d8f7c
rewine <luhongxu@deepin.org>
2025-06-24 14:41:06 +0800

    chore: configure translation files and update Chinese translations
    1. Add .tx/config file to set up Transifex translation management with
    multiple resource configurations for different components
    2. Update Simplified Chinese translations in wallpaper plugin by
    removing 'type="unfinished"' from several entries
    3. Update Simplified Chinese translations in lockscreen plugin by
    removing 'type="unfinished"' from multiple translation tags
    
    These changes improve translation management workflow through proper
    Transifex configuration and ensure the Chinese translations are marked
    as completed rather than unfinished.
    
    chore: 配置翻译文件并更新中文翻译
    
    1. 添加 .tx/config 文件，配置多个组件的 Transifex 翻译资源管理
    2. 在壁纸插件中更新简体中文翻译，移除多个条目中的 'type="unfinished"'
    3. 在锁屏插件中更新简体中文翻译，移除多个标签中的 'type="unfinished"'
    
    这些更改通过正确的 Transifex 配置改进了翻译管理工作流程，并确保中文翻译
    标记为已完成状态而非未完成状态。

--- src/plugins/lockscreen/translations/lockscreen.zh_CN.ts
+++ src/plugins/lockscreen/translations/lockscreen.zh_CN.ts
@@ -37,7 +37,7 @@
     <message>
         <location filename="../qml/ControlAction.qml" line="55"/>
         <source>Power</source>
-        <translation type="unfinished">电源</translation>
+        <translation>电源</translation>
     </message>
     <message>
         <source>lock</source>
@@ -64,7 +64,7 @@
     <message>
         <location filename="../qml/LockView.qml" line="176"/>
         <source>Password is incorrect.</source>
-        <translation type="unfinished">密码错误</translation>
+        <translation>密码错误</translation>
     </message>
 </context>
 <context>
@@ -106,17 +106,17 @@
     <message>
         <location filename="../qml/ShutdownView.qml" line="32"/>
         <source>lock</source>
-        <translation type="unfinished">锁定</translation>
+        <translation>锁定</translation>
     </message>
     <message>
         <location filename="../qml/ShutdownView.qml" line="37"/>
         <source>switch user</source>
-        <translation type="unfinished">切换帐户</translation>
+        <translation>切换帐户</translation>
     </message>
     <message>
         <location filename="../qml/ShutdownView.qml" line="43"/>
         <source>Logout</source>
-        <translation type="unfinished">注销</translation>
+        <translation>注销</translation>
     </message>
 </context>
 <context>
@@ -140,7 +140,7 @@
     <message>
         <location filename="../qml/UserInput.qml" line="141"/>
         <source>Password</source>
-        <translation type="unfinished">密码</translation>
+        <translation>密码</translation>
     </message>
 </context>
 </TS>
