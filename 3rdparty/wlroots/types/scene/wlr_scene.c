82fc3c6fd467be4b146db30daeae46e7c17857f3
Alexander Orzechowski <alex@ozal.ski>
2026-01-07 12:06:54 -0500

    wlr_scene: Don't recurse when disabling scene node if a child is already disabled
    If a child is disabled, that means the node already went through the
    rigamarole of cleaning up its state. Don't do it again.

--- 3rdparty/wlroots/types/scene/wlr_scene.c
+++ 3rdparty/wlroots/types/scene/wlr_scene.c
@@ -671,6 +671,10 @@ static void scene_node_cleanup_when_disabled(struct wlr_scene_node *node, bool x
 		struct wlr_scene_tree *scene_tree = wlr_scene_tree_from_node(node);
 		struct wlr_scene_node *child;
 		wl_list_for_each(child, &scene_tree->children, link) {
+			if (!child->enabled) {
+				continue;
+			}
+
 			scene_node_cleanup_when_disabled(child, xwayland_restack);
 		}
 		return;
