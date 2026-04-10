843460ec5275e014b2ffa5a749961cca601aa77c
rewine <luhongxu@deepin.org>
2026-04-10 17:22:11 +0800

    chore: refine decoration handling logic
    Log: Streamline SSD decoration toggling in Helper and simplify wxdg decoration mode updates.

--- waylib/src/server/protocols/wxdgdecorationmanager.cpp
+++ waylib/src/server/protocols/wxdgdecorationmanager.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// Copyright (C) 2023-2026 rewine <luhongxu@deepin.org>.
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "wxdgdecorationmanager.h"
@@ -78,7 +78,6 @@ void WXdgDecorationManagerPrivate::updateDecorationMode(qw_xdg_toplevel_decorati
             break;
         default:
             Q_UNREACHABLE();
-            break;
     }
     if (mode == WXdgDecorationManager::None) {
         mode = preferredMode;
@@ -93,12 +92,9 @@ void WXdgDecorationManagerPrivate::updateDecorationMode(qw_xdg_toplevel_decorati
                 break;
             default:
                 Q_UNREACHABLE();
-                break;
         }
-        decorations.insert(surface, preferredMode);
-    } else {
-        decorations.insert(surface, mode);
     }
+    decorations.insert(surface, mode);
 
     Q_EMIT q->surfaceModeChanged(surface, mode);
 }
