c348e63789f47b7669c1e16876149e7689231785
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-04-29 14:03:58 +0800

    feat: refactor treeland-screensaver-v1 protocol implementation
    implemented on top of QtWaylandServer generated protocol classes.
    
    Log: refactor treeland-screensaver-v1 protocol implementation.
    Tasks:
    
    Influence:
    1. Verify behavior for treeland-screensaver-v1.

--- src/modules/screensaver/screensaverinterfacev1.h
+++ src/modules/screensaver/screensaverinterfacev1.h
@@ -1,21 +1,28 @@
-// Copyright (C) 2025 UnionTech Software Technology Co., Ltd.
+// Copyright (C) 2025-2026 UnionTech Software Technology Co., Ltd.
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "wserver.h"
 
-/**
- * Simple idle inhibit protocol.
- */
-class ScreensaverInterfaceV1 : public Waylib::Server::WServerInterface {
-    struct wl_global *m_global { nullptr };
-    QHash<wl_resource*, std::tuple<QByteArray, QByteArray>> m_inhibits;
+WAYLIB_SERVER_USE_NAMESPACE
+
+class ScreensaverInterfaceV1Private;
+class ScreensaverInterfaceV1 : public QObject , public WServerInterface
+{
+    Q_OBJECT
 public:
+    explicit ScreensaverInterfaceV1(QObject *parent = nullptr);
+    ~ScreensaverInterfaceV1() override;
+
     QByteArrayView interfaceName() const override;
-    void inhibit(wl_resource *res, const char *appName, const char *reason);
-    void uninhibit(wl_resource *res);
-    inline bool isInhibited() const { return !m_inhibits.isEmpty(); }
+    bool isInhibited() const;
+
+    static constexpr int InterfaceVersion = 1;
+
 protected:
-    void create(WAYLIB_SERVER_NAMESPACE::WServer *server) override;
-    void destroy(WAYLIB_SERVER_NAMESPACE::WServer *server) override;
-    inline wl_global *global() const override { return m_global; }
+    void create(WServer *server) override;
+    void destroy(WServer *server) override;
+    wl_global *global() const override;
+
+private:
+    std::unique_ptr<ScreensaverInterfaceV1Private> d;
 };
