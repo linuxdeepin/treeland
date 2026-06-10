e0d6dd83bfb5fb7ce99adb68fd94f9142b9498ce
deepin-wm <treeland@uniontech.com>
2026-06-10 17:24:46 +0800

    feat: use DRM format for texture creation

--- waylib/src/server/qtquick/woutputrenderwindow.h
+++ waylib/src/server/qtquick/woutputrenderwindow.h
@@ -54,6 +54,11 @@ public:
     QW_NAMESPACE::qw_renderer *renderer() const;
     QW_NAMESPACE::qw_allocator *allocator() const;
 
+    // Convenience methods for DRM format texture creation
+    wlr_egl *egl() const;
+    QRhiTexture *createTextureFromDRMFormat(uint32_t drmFormat, const QSize &size);
+    QRhiTexture *createMatchingTexture(QSGTexture *sourceTexture);
+
     qreal width() const;
     qreal height() const;
     WBufferRenderer *currentRenderer() const;
