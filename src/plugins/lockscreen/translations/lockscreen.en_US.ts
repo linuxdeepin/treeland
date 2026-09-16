415a0c3a3d71f6eae122da0e76561c9ed569f7f0
liujiangtao <liujiangtao1@uniontech.com>
2026-09-16 16:33:36 +0800

    feat(lockscreen): add power actions to shutdown view
    Merge PowerList into ShutdownView with shutdown, reboot,
    suspend, hibernate and lock/switch/logout buttons. Adjust
    focus logic to skip hidden or disabled buttons when opening
    the power panel.
    
    将关机面板重构成ShutdownView，支持关机/重启/挂起/休眠
    及锁屏，切换用户/注销操作，移除原PowerList组件。打开
    电源面板时聚焦逻辑会跳过不可见或不可用的按钮。
    
    Log: 重构锁屏关机面板并增加电源操作按钮
    PMS: TASK-390751
    Influence: 锁屏关机界面提供关机、重启、挂起、休眠等完整
    电源操作；电源面板打开时聚焦第一个可见且可用的按钮。交互
    和键盘焦点导航相应调整。

--- src/plugins/lockscreen/translations/lockscreen.en_US.ts
+++ src/plugins/lockscreen/translations/lockscreen.en_US.ts
@@ -18,44 +18,41 @@
     </message>
 </context>
 <context>
-    <name>PowerList</name>
-    <message>
-        <location filename="../qml/PowerList.qml" line="58"/>
-        <source>Hibernate</source>
-        <translation>Hibernate</translation>
-    </message>
-    <message>
-        <location filename="../qml/PowerList.qml" line="49"/>
-        <source>Suspend</source>
-        <translation>Suspend</translation>
-    </message>
-    <message>
-        <location filename="../qml/PowerList.qml" line="42"/>
-        <source>Reboot</source>
-        <translation>Reboot</translation>
-    </message>
-    <message>
-        <location filename="../qml/PowerList.qml" line="35"/>
-        <source>Shut Down</source>
-        <translation>Shut Down</translation>
-    </message>
-</context>
-<context>
     <name>ShutdownView</name>
     <message>
-        <location filename="../qml/ShutdownView.qml" line="31"/>
+        <location filename="../qml/ShutdownView.qml" line="46"/>
         <source>lock</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/ShutdownView.qml" line="37"/>
+        <location filename="../qml/ShutdownView.qml" line="55"/>
         <source>switch user</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/ShutdownView.qml" line="44"/>
+        <location filename="../qml/ShutdownView.qml" line="66"/>
         <source>Logout</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownView.qml" line="76"/>
+        <source>Shut Down</source>
+        <translation>Shut Down</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownView.qml" line="87"/>
+        <source>Reboot</source>
+        <translation>Reboot</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownView.qml" line="97"/>
+        <source>Suspend</source>
+        <translation>Suspend</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownView.qml" line="107"/>
+        <source>Hibernate</source>
+        <translation>Hibernate</translation>
     </message>
 </context>
 <context>
